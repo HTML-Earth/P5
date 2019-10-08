ec9847089340e22770f410b8f87e48bdd2a646bb
Stefan Mir-Mackiewicz <htmlearth@gmail.com>
2019-10-08 14:43:59 +0200

    Added Unity ML-Toolkit

---
+++ ml-agents-0.10.0/ml-agents-envs/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name="mlagents_envs",
+    version="0.10.0",
+    description="Unity Machine Learning Agents Interface",
+    url="https://github.com/Unity-Technologies/ml-agents",
+    author="Unity Technologies",
+    author_email="ML-Agents@unity3d.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["mlagents.envs", "mlagents.envs.communicator_objects"],  # Required
+    zip_safe=False,
+    install_requires=[
+        "cloudpickle",
+        "grpcio>=1.11.0",
+        "numpy>=1.13.3,<2.0",
+        "Pillow>=4.2.1",
+        "protobuf>=3.6",
+    ],
+    python_requires=">=3.5",
+)
---
+++ ml-agents-0.10.0/ml-agents/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_namespace_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="mlagents",
+    version="0.10.0",
+    description="Unity Machine Learning Agents",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Unity-Technologies/ml-agents",
+    author="Unity Technologies",
+    author_email="ML-Agents@unity3d.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # find_namespace_packages will recurse through the directories and find all the packages
+    packages=find_namespace_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    zip_safe=False,
+    install_requires=[
+        # Test-only dependencies should go in test_requirements.txt, not here.
+        "grpcio>=1.11.0",
+        "h5py>=2.9.0",
+        "jupyter",
+        "matplotlib",
+        "mlagents_envs==0.10.0",
+        "numpy>=1.13.3,<2.0",
+        "Pillow>=4.2.1",
+        "protobuf>=3.6",
+        "pyyaml",
+        "tensorflow>=1.7,<2.0",
+        'pypiwin32==223;platform_system=="Windows"',
+    ],
+    python_requires=">=3.6.1",
+    entry_points={"console_scripts": ["mlagents-learn=mlagents.trainers.learn:main"]},
+)

8c21eb8b3d2ff40d8e19aef2a3fcbb8d0fa9dff8
HTML_Earth <htmlearth@gmail.com>
2019-11-13 12:49:27 +0100

    eh

--- ml-agents-0.11.0/project/test.py
+++ ml-agents-0.11.0/project/test.py
@@ -1,10 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
 from mlagents.envs.environment import UnityEnvironment
 
 env = UnityEnvironment(file_name=None, worker_id=0, seed=1)
 
 #env.step(action, memory=None, text_action=None)
-
-print(str(env))
 
 env.reset(train_mode=True, config=None)
 

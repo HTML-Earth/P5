bd2e1ff4bdf36daea3f33740617959fab2a2460a
Cuz <37619846+Rynke@users.noreply.github.com>
2019-11-20 12:39:43 +0100

    DropZone Added

---
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/DropZone.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour
+{
+    public float radius = 5f;
+
+    void OnDrawGizmos()
+    {
+        Handles.DrawSolidDisc(transform.position, Vector3.up, radius);
+    }
+
+}

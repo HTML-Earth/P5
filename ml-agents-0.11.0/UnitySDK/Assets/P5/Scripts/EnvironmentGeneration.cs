91da33767806211cd31bf5f18c20cbd4de0e2e99
Cuz <37619846+Rynke@users.noreply.github.com>
2019-11-20 15:41:09 +0100

    Environment Generation: Dropzone
    -random
    -cannot spawn on robot
    
    Co-Authored-By: Simon Mathiasen <smath17@users.noreply.github.com>

--- ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/EnvironmentGeneration.cs
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/EnvironmentGeneration.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class EnvironmentGeneration : MonoBehaviour
 {
-
+    float robot_x;
+    float robot_z;
+    float dropzone_x;
+    float dropzone_z;
+    
     public GameObject debrisPrefab;
     public Transform dropzone;
     public Transform robot;
@@ -18,12 +22,14 @@ public class EnvironmentGeneration : MonoBehaviour
         GenerateDebris();
     }
 
+    // Robot start position and rotation
     private void PlaceRobot()
     {
-        //float x = Random.Range(-20f, 20f);
-        //float z = Random.Range(-20f, 20f);
-        float x = -20f + Random.Range(0, 5) * 8;
-        float z = -20f + Random.Range(0, 5) * 8;
+        robot_x = Random.Range(0, 5);
+        robot_z = Random.Range(0, 5);
+        
+        float x = -20f + robot_x * 8;
+        float z = -20f + robot_z * 8;
         float randomRotation = Random.Range(0, 3) * 90f;
         robot.position = new Vector3(x, 0.9f, z);
         robot.rotation = Quaternion.Euler(0, randomRotation, 0);
@@ -37,8 +43,17 @@ public class EnvironmentGeneration : MonoBehaviour
     {
     }
 
+    // Dropzone position
     public void GenerateDropzone()
     {
-        
+        do
+        {
+            dropzone_x = Random.Range(0, 5);
+            dropzone_z = Random.Range(0, 5);
+        } while (robot_x == dropzone_x && robot_z == dropzone_z);
+
+        float x = -20f + Random.Range(0, 5) * 8;
+        float z = -20f + Random.Range(0, 5) * 8;
+        dropzone.position = new Vector3(x, 0.2f, z);
     }
 }

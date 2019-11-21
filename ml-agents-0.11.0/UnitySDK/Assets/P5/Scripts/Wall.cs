ed1b9de3a080c107a65365707e1f6e5a63629ce3
Ruofan <rwu17@student.aau.dk>
2019-11-21 14:24:54 +0100

    "Random" Wall Size
    "Random" means that the script is sheit.

---
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/Wall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Wall : MonoBehaviour
+{
+    public float width, length, depth;
+    public int maxWidth = 10;
+    public int maxLength = 10;
+    public int maxDepth = 1;
+    public float maxScale = 0.75f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomWallSize();
+    }
+
+    private void RandomWallSize()
+    {
+        width = Random.Range(1, maxWidth) * Random.Range(0, maxScale);
+        length = Random.Range(1, maxLength) * Random.Range(0, maxScale);
+        depth = Random.Range(1, maxDepth) /** Random.Range(0, maxScale)*/;
+        
+        transform.localScale = new Vector3(width, length, depth);        
+    }
+
+}

459026a177e2ae51bc6c6452e3052061bffcb129
Cuz <37619846+Rynke@users.noreply.github.com>
2019-11-21 12:07:30 +0100

    Update EnvironmentGeneration.cs
    - Generate walls almost done

--- ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/EnvironmentGeneration.cs
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/EnvironmentGeneration.cs
@@ -25,18 +25,35 @@ public class EnvironmentGeneration : MonoBehaviour
     // Robot start position and rotation
     private void PlaceRobot()
     {
-        robot_x = Random.Range(0, 5);
-        robot_z = Random.Range(0, 5);
+        robot_x = Random.Range(0, 6);
+        robot_z = Random.Range(0, 6);
         
         float x = -20f + robot_x * 8;
         float z = -20f + robot_z * 8;
-        float randomRotation = Random.Range(0, 3) * 90f;
+        float randomRotation = Random.Range(0, 4) * 90f;
         robot.position = new Vector3(x, 0.9f, z);
         robot.rotation = Quaternion.Euler(0, randomRotation, 0);
     }
 
     private void GenerateWalls()
     {
+        int amount = Random.Range(4, 9);
+        int x;
+        int z;
+        List<int> xList = new List<int>();
+        List<int> zList = new List<int>();
+        for (int i = 0; i < amount; i++)
+        {
+            do
+            {
+                x = Random.Range(0, 4);
+                z = Random.Range(0, 4);
+            } while ();
+            var rotation = Random.Range(0, 2) * 90f;
+            Instantiate(wall, new Vector3(-16f + x * 8, 2.375f, -16f + z * 8), Quaternion.Euler(0f, rotation, 0f));
+            xList.Add(x);
+            zList.Add(z);
+        }
     }
 
     private void GenerateDebris()
@@ -48,12 +65,12 @@ public class EnvironmentGeneration : MonoBehaviour
     {
         do
         {
-            dropzone_x = Random.Range(0, 5);
-            dropzone_z = Random.Range(0, 5);
+            dropzone_x = Random.Range(0, 6);
+            dropzone_z = Random.Range(0, 6);
         } while (robot_x == dropzone_x && robot_z == dropzone_z);
 
-        float x = -20f + Random.Range(0, 5) * 8;
-        float z = -20f + Random.Range(0, 5) * 8;
+        float x = -20f + Random.Range(0, 6) * 8; // -20 to +20
+        float z = -20f + Random.Range(0, 6) * 8;
         dropzone.position = new Vector3(x, 0.2f, z);
     }
 }

43228ecd33b84097851406b5c35bfd41105c11d0
Ruofan <rwu17@student.aau.dk>
2019-11-22 11:35:19 +0100

    Hotfix: Debris

--- ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/Debris.cs
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/Debris.cs
@@ -6,21 +6,38 @@ using Random = UnityEngine.Random;
 
 public class Debris : MonoBehaviour
 {
-    public float width, length, depth;
     public int maxWidth = 10;
     public int maxLength = 10;
-    public int maxDepth = 10;
-    public float maxScale = 0.25f /*to pay respect*/;
+    public int maxHeight = 10;
+    public float maxScale = 0.25f;
     private void Start()
     {
         RandomDebris();
     }
     
-    private void RandomDebris() {
-        width = Random.Range(1, maxWidth) * Random.Range(0, maxScale);
-        length = Random.Range(1, maxLength) * Random.Range(0, maxScale);
-        depth = Random.Range(1, maxDepth) * Random.Range(0, maxScale);
+    private void RandomDebris()
+    {
+        float width = transform.localScale.x;
+        float height = transform.localScale.y;
+        float length = transform.localScale.z;
+
+        float minWidth = Random.Range(0, maxWidth);
+        float minHeight = Random.Range(0, maxHeight);
+        float minLength = Random.Range(0, maxLength);
+        float minScale = Random.Range(0, maxScale);
         
-        transform.localScale = new Vector3(width, length, depth);
+        do
+        {
+            minWidth = Random.Range(0, maxWidth);
+            minHeight = Random.Range(0, maxHeight);
+            minLength = Random.Range(0, maxLength);
+            minScale = Random.Range(0, maxScale);
+        } while (minWidth == 0 || minHeight == 0 || minLength == 0 || minScale == 0);
+
+        width = Random.Range(minWidth, maxWidth) * Random.Range(0, maxScale);
+        height = Random.Range(minHeight, maxHeight) * Random.Range(0, maxScale);
+        length = Random.Range(minLength, maxLength) * Random.Range(0, maxScale);
+
+        transform.localScale = new Vector3(width, length, length);
     }
 }
--- ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/Wall.cs
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/Wall.cs
@@ -7,8 +7,14 @@ using Random = UnityEngine.Random;
 public class Wall : MonoBehaviour
 {
     public float length;
+    /*
+     * the minimum and maximum value of length
+     * is found by the size of the entire
+     * platform in grid format
+     */
+    
     public float minLength = 4.0f;
-    public float maxLength = 8.0f;
+    public float maxLength = 8.0f; 
     
     void Start()
     {

36ea06ceb03bd22b4b6bacefd9293826b1706f08
GarridoY <dgmn17@student.aau.dk>
2019-11-22 15:17:44 +0100

    Comments in RobotVision

--- ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/RobotVision.cs
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/RobotVision.cs
@@ -25,13 +25,18 @@ public class RobotVision : MonoBehaviour
 
     public void InitializeDebrisArray()
     {
+        // Get list of Debris
         List<Transform> debrisList = academy.GetDebris();
 
+        // Initialize array of Transform for every debris
         debrisArray = new Transform[debrisList.Count];
+        // Initialize array of Bools for visibility of every debris
         visibility = new bool[debrisList.Count];
-
+        // Initialize array of Vectors for positions of every debris
         knownPositions = new Vector3[debrisList.Count];
 
+        // Put every debris in the list in the array
+        // Give every debris a position of "positive infinity"
         for (int debris = 0; debris < debrisArray.Length; debris++)
         {
             debrisArray[debris] = debrisList[debris];
@@ -48,16 +53,23 @@ public class RobotVision : MonoBehaviour
         
         Vector3 currentSensorPosition = sensorPosition.position;
         
+        // For every debris -> check if it is visible
         for (int debris = 0; debris < debrisArray.Length; debris++)
         {
+            // Current debris-position
             Vector3 currentDebrisPosition = debrisArray[debris].position;
 
+            // Vector from Sensor-position to current Debris-position
             Vector3 directionToDebris = currentDebrisPosition - currentSensorPosition;
 
+            // Calculate angle between normalized Sensor-position and normalized Debris-position
+            // If it is less than 60 degrees -> continue
             if (Vector3.Angle(sensorPosition.forward, directionToDebris.normalized) < visionAngle * 0.5f)
             {
                 Ray ray = new Ray(currentSensorPosition, directionToDebris);
 
+                // Cast a ray from the Sensor-position to Debris-position
+                // If raycast from Robot to Debris gets blocked
                 if (Physics.Raycast(ray, out RaycastHit hitInfo,
                     Vector3.Distance(currentSensorPosition, currentDebrisPosition), layerMask))
                     visibility[debris] = false;
@@ -76,6 +88,7 @@ public class RobotVision : MonoBehaviour
         return visibility;
     }
 
+    // Return positions of visible debris
     public Vector3[] GetKnownPositions()
     {
         for (int debris = 0; debris < debrisArray.Length; debris++)

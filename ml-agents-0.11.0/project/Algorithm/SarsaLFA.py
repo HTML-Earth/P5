661fb07a470485cbce146b5d5244ba5b733779d9
HTML_Earth <htmlearth@gmail.com>
2019-11-26 12:39:12 +0100

    SarsaLFA pseudocode

---
+++ ml-agents-0.11.0/project/Algorithm/SarsaLFA.py
@@ -0,0 +1,21 @@
+# SARSA_LFA(F, gamma, eta)
+#   Inputs
+#       F = <F_1, ... , F_n>: a set of features. Define F_0(s,a) = 1.
+#       gamma ∈ [0,1]: discount factor
+#       eta > 0: step size for gradient descent
+#
+#   Local
+#       weights w = <w_0, ... , w_n>, initialized arbitrarily
+#
+#   observe current state s
+#   select action a
+#   repeat
+#       do(a)
+#       observe reward r and state s'
+#       select action a' (using policy based on Q_w)
+#       delta := r + gamma * Q_w(s', a') - Q_w(s,a)
+#       for i = 0 to n do
+#           w_i := w_i + eta * delta * F_i(s,a)
+#       s := s'
+#       a := a'
+#   until termination

c101d011b8dfb6466e9854da69a64350bffa53fc
Cuz <37619846+Rynke@users.noreply.github.com>
2019-11-27 13:33:54 +0100

    Timer i Unity

---
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private float theTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theTime = Time.time;
+        string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+        string seconds = (theTime % 60).ToString("00");
+        string milliseconds = theTime.ToString ("#.##");
+        text.text = minutes + ":" + seconds + ":" + milliseconds.Substring(milliseconds.IndexOf(',') + 1);;
+    }
+}

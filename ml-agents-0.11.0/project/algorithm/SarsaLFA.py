f1f6a9e8c26875a052d9f01c1a85412ec3b3b5f1
GarridoY <dgmn17@student.aau.dk>
2019-11-29 12:01:13 +0100

    Working algorithm
    Start AgentController and Press Play in Unity

--- ml-agents-0.11.0/project/algorithm/SarsaLFA.py
+++ ml-agents-0.11.0/project/algorithm/SarsaLFA.py
@@ -27,12 +27,11 @@ import random
 
 class Sarsa:
 
-    def __init__(self, gamma=0.9, eta=0.1):
+    def __init__(self, gamma=0.9, eta=0.6):
         # Q function
         self.q = {}
-
         # Feature function
-        self.feature = {}
+        self.feature_values = [0 for i in range(0, 10)]
 
         self.gamma = gamma
         self.eta = eta
@@ -43,47 +42,48 @@ class Sarsa:
     def get_q_value(self, state, action):
         # Return value for given state and action
         # 0.0 is default if there exist no value for given state and action
-        return self.q.get((state, action), 0.0)
+        return self.q.get((tuple(state), action), 0.0)
 
     def get_feature_value(self, state, action):
         observations = state
-        velocity_z = observations[5] # z means the velocity when moving forward and backward.
+
+        # z means the velocity when moving forward and backward.
+        velocity_z = observations[5]
         sensors_front = [observations[11], observations[12], observations[40]]
         sensors_behind = [-observations[26], -observations[25], -observations[27]]
 
-        featureValues = []
-
         # Check if robot is throttling into wall
         if sensors_front < [velocity_z]:
-            featureValues.append(1)
+            self.feature_values[0] = 1
         else:
-            featureValues.append(0)
+            self.feature_values[0] = 0
 
         # Check if robot is reversing into wall
         if sensors_behind > [velocity_z]:
-            featureValues.append(1)
+            self.feature_values[1] = 1
         else:
-            featureValues.append(0)
+            self.feature_values[1] = 0
 
         # Check if robot is within the dropzone
         if observations[60]:
-            featureValues.append(1)
+            self.feature_values[2] = 1
         else:
-            featureValues.append(0)
+            self.feature_values[2] = 0
 
         # Check if robot is getting closer to debris
+        index = 3
         for i in range(61, 67):
             if observations[i]:
-                featureValues.append(1)
+                self.feature_values[index] = 1
             else:
-                featureValues.append(0)
+                self.feature_values[index] = 0
+            index += 1
 
-    # Check if ready to pickup debris
-        if observations.append(6) == 330 and observations.append(7) == 360 - 47:
-            featureValues.append(1)
+        # Check if ready to pickup debris
+        if observations[6] == 330 and observations[7] == 360 - 47:
+            self.feature_values[index] = 1
         else:
-            featureValues.append(0)
-        return featureValues
+            self.feature_values[index] = 0
 
     def choose_action(self, state, action):
         if random.random() < self.eta:
@@ -101,14 +101,7 @@ class Sarsa:
             action = self.actions[i]
         return action
 
-    def sarsa_lfa(self, agent, feature_tuple):
-        # Arbitrarily initalize weights which will be updated later
-        # TODO Assume that there is an extra feature F0(s,a) whose value is always 1,
-        #  so that w0 is not a special case.
-        weights = []
-        for i in range(len(feature_tuple)):
-            weights.append(random.randint(0, 5))
-
+    def sarsa_lfa(self, agent):
         # Update observations to make sure current state is correct
         agent.update_observations()
 
@@ -117,20 +110,35 @@ class Sarsa:
         # Select action a
         cur_action = (1, 0, 0, 0)
 
+        self.get_feature_value(cur_state, cur_action)
+
+        # Arbitrarily initalize weights which will be updated later
+        # TODO Assume that there is an extra feature F0(s,a) whose value is always 1,
+        #  so that w0 is not a special case.
+        weights = []
+        for i in range(len(self.feature_values)):
+            weights.append(random.randint(0, 5))
+
         while True:
             # Perform action and observe state s'
-            new_state = agent.perform_action(cur_action)
+            new_state = agent.perform_action(*cur_action).get('Robot').vector_observations[0]
             # Observe reward r
-            reward = agent.rewards
+            reward = agent.get_reward()[0]
             # Select action a' (using a policy based on the Q-function
             new_action = self.choose_action(cur_state, cur_action)
+
+            print("Chosen action: {:4s}" + str(new_action))
+            print("Reward: " + str(reward))
 
             # TODO Explain what delta is
             delta = reward + self.gamma * self.get_q_value(new_state, new_action) - self.get_q_value(cur_state, cur_action)
 
+            # Update features
+            self.get_feature_value(cur_state, cur_action)
+
             # Update each weight for each feature
-            for i in range(len(feature_tuple)):
-                weights[i] = weights[i] + self.eta * delta * feature_tuple[i].get(cur_state, cur_action)
+            for i in range(len(self.feature_values)):
+                weights[i] = weights[i] + self.eta * delta * self.feature_values[i]
 
             # Update state and action so these will be performed
             cur_state = new_state

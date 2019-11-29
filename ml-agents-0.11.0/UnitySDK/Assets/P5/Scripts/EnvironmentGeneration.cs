05a5eac51857ffb1b7e377e11b3b7098083fee2b
Cuz <37619846+Rynke@users.noreply.github.com>
2019-11-29 14:50:59 +0100

    Wall update
    - Walls no longer float
    - Walls are shorter
    
    fixes #17

--- ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/EnvironmentGeneration.cs
+++ ml-agents-0.11.0/UnitySDK/Assets/P5/Scripts/EnvironmentGeneration.cs
@@ -73,7 +73,7 @@ public class EnvironmentGeneration : MonoBehaviour
                 z = Random.Range(0, 5);
             } while (SetInLists(x, z, xList, zList));
             var rotation = Random.Range(0, 2) * 90f;
-            GameObject obstacle_wall = Instantiate(wall, new Vector3(-16f + x * 8, 2.375f, -16f + z * 8), Quaternion.Euler(0f, rotation, 0f));
+            GameObject obstacle_wall = Instantiate(wall, new Vector3(-16f + x * 8, 1.25f, -16f + z * 8), Quaternion.Euler(0f, rotation, 0f));
             
             environmentObjects.Add(obstacle_wall);
             

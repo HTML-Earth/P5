f01ef59e5d362a76181f27c24f44e22720317790
Simon Mathiasen <33898979+smath17@users.noreply.github.com>
2019-12-15 21:52:26 +0100

    Starting from the bottom

---
+++ ml-agents-0.11.0/project/Old_Sarsa/Main.py
@@ -0,0 +1,62 @@
+from Old_Sarsa.Sarsa import SarsaLFA
+import sys
+
+# python3.6 Main.py -[mode] [training-file]
+# python3.6 Main.py -help
+if __name__ == '__main__':
+    training_mode = 1
+    training_file_name = ""
+    build_scene = None
+
+    if len(sys.argv) > 1:
+        # New training
+        if sys.argv[1] == "-n":
+            training_mode = 1
+
+            # Build scene
+            if not sys.argv[2] is None:
+                build_scene = sys.argv[2]
+
+        # Continue training
+        elif sys.argv[1] == "-c":
+            training_mode = 2
+
+            # Build scene and training file
+            if len(sys.argv) == 4:
+                # Build scene
+                if not sys.argv[2] is None:
+                    build_scene = sys.argv[2]
+
+                # Training file
+                if not sys.argv[3] is None:
+                    training_file_name = sys.argv[3]
+            
+            # Training file
+            elif len(sys.argv) == 3:
+                training_file_name = sys.argv[2]
+            else:
+                raise Exception("Missing training-file or build scene argument.")
+                
+
+        elif sys.argv[1] == "-help":
+            print("{:35s} Sets mode to 'training mode'. \n"
+                  .format("-n [Optional: path to build scene]"))
+
+            print("{:35s} Sets mode to 'training mode' with given training file name. "
+                  "\n{:35s} Training file needs to be inside 'training-files' folder. \n"
+                  .format("-c [Build scene] [training-file]", ''))
+
+            print("{:35s} Example of new training session:\n "
+                  "{:35s}python3.6 Main.py -n 1x3_scene.app\n\n"
+                  "{:35s} Example of continued training session:\n "
+                  "{:35s}python3.6 Main.py -c 1x3_scene.app 1_training.txt"
+                  .format("", "", "", ""))
+            sys.exit(0)
+
+    # Algorithm object
+    sarsa = SarsaLFA(build_scene)
+
+    if training_mode == 1:
+        sarsa.new_training_agent()
+    elif training_mode == 2:
+        sarsa.continue_training_agent(training_file_name)
---
+++ ml-agents-0.11.0/project/Old_Sarsa/Sarsa.py
@@ -0,0 +1,137 @@
+import random
+from Old_Sarsa.File_manager import TrainingFileManager
+from Old_Sarsa.AgentController import Agent
+import matplotlib.pyplot as plt
+
+
+class SarsaLFA:
+
+    def __init__(self, build_scene, gamma=0.99, eta=0.9, epsilon=0.3):
+        self.agent = Agent()
+        self.agent.setup_connection_with_unity(build_scene)
+        self.training_file_manager = TrainingFileManager()
+
+        # Q-function
+        self.q_function = {}
+
+        # Variables
+        self.gamma = gamma
+        self.eta = eta
+        self.epsilon = epsilon
+
+        # Weights
+        self.weights = []
+
+        # Rewards
+        self.reward_per_episode = 0
+
+        self.episode = 1
+
+    def lookup_q(self, state, action):
+        return self.q_function.get((tuple(state), action), 0.0)
+
+    def get_q_value(self, state, action):
+        total_q_value = 0
+
+        for i in range(0, len(self.weights)):
+            total_q_value += self.weights[i] * self.agent.features[i](state, action)
+
+        self.q_function[(tuple(state), action)] = total_q_value
+        return total_q_value
+
+    def choose_action(self, state):
+        actions = self.agent.actions
+
+        if random.random() < self.epsilon:
+            action = (random.randint(0, 2), random.randint(0, 2))
+        else:
+            q = [self.lookup_q(state, a) for a in actions]
+            max_q = max(q)
+            count = q.count(max_q)
+            if count > 1:
+                best = [i for i in range(len(actions)) if q[i] == max_q]
+                i = random.choice(best)
+            else:
+                i = q.index(max_q)
+
+            action = actions[i]
+        return action
+
+    def train_agent(self):
+        state = self.agent.get_state()
+        action = (0, 0)
+
+        # Variables for x- and y- coordinates
+        x_episode = []
+        y_delta = []
+        y_reward = []
+
+        while self.episode <= 100:
+            self.agent.perform_action(*action)
+
+            new_state = self.agent.get_state()
+            reward = self.agent.get_reward()
+            new_action = self.choose_action(state)
+
+            # Calculate data point for linear regression
+            delta = reward + self.gamma * self.get_q_value(new_state, new_action) - self.get_q_value(state, action)
+
+            # Update weights
+            for i in range(0, len(self.weights)):
+                self.weights[i] = self.weights[i] + self.eta * delta * self.agent.features[i](state, action)
+
+            state = new_state
+            action = new_action
+
+            self.reward_per_episode += reward
+
+            if self.agent.is_done():
+                # Save x- and y- values
+                x_episode.append(self.episode)
+                y_delta.append(delta)
+                y_reward.append(self.reward_per_episode)
+
+                self.training_file_manager.save_episode_rewards(self.episode, self.reward_per_episode)
+
+                self.episode += 1
+                self.reward_per_episode = 0
+                #TODO: I think this may cause some conflict with AgentReset where we also reset the environment
+                #self.agent.reset_simulation()
+
+        # Delta in relation to episodes
+        plt.subplot(2, 1, 1)
+        plt.plot(x_episode, y_delta)
+        plt.ylabel('y - Delta')
+
+        # Reward in relation to episodes
+        plt.subplot(2, 1, 2)
+        plt.plot(x_episode, y_reward)
+        plt.ylabel('y - Reward')
+        plt.xlabel('x - Episode')
+        plt.title('Delta and Reward in relation to Episode')
+
+        plt.show()
+
+        # Save weights and Q-function
+        self.training_file_manager.save_values(self.weights, self.q_function)
+
+    def new_training_agent(self):
+        # Create reward file
+        self.training_file_manager.create_episode_file()
+
+        # Create training file
+        self.training_file_manager.create_training_file()
+
+        for i in range(0, len(self.agent.features)):
+            self.weights.append(random.random())
+
+        self.train_agent()
+
+    def continue_training_agent(self, training_file_name):
+        # Set path to training file name
+        self.training_file_manager.set_training_file(training_file_name)
+
+        self.weights = self.training_file_manager.read_weights()
+        self.q_function = self.training_file_manager.read_q_function()
+
+        self.train_agent()
